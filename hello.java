f3562e02c08c4d29840eb715eec8446d9d244cae
Heba Sayed <hebasayed1603@gmail.com>
2023-09-28 20:40:47 -0400

    Changed loop to 15

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
 public class hello {
 	public static void main(String[] args) {
 
-		for(int i=1;i<=12;i++) {
+		for (int i = 1; i <= 15; i++) {
 
-			System.out.println("Hello World..."+i);
+			System.out.println("Hello World..." + i);
 		}
 	}
 }
